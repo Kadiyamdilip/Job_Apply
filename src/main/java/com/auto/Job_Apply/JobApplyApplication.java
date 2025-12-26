ee05ac96b9c52419b3162c7ed4f10df647245902
Kadiyamdilip <88888585+Kadiyamdilip@users.noreply.github.com>
2025-12-26 15:08:56 -0500

    created new project
    created new project

---
+++ src/main/java/com/auto/Job_Apply/JobApplyApplication.java
@@ -0,0 +1,13 @@
+package com.auto.Job_Apply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobApplyApplication.class, args);
+	}
+
+}
